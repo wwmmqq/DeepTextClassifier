3eb6f8b56acbd2ca4e052d177c734c3a20ae0f36
Maoquan <myqway@outlook.com>
2017-11-04 16:37:05 +0800

    cnn-gru

--- src/config.py
+++ src/config.py
@@ -66,3 +66,35 @@ class ConfigRnn(object):
     max_gradient_norm = 10
     learning_rate = 1e-3
     l2_rate = 1e-4
+
+
+class ConfigRnnCnn(object):
+    # [General]
+    _HOME_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    data_name = "qc"    # nlpcc2017
+
+    train_file = _HOME_PATH + "/data/{}/train.txt".format(data_name)
+    test_file = _HOME_PATH + "/data/{}/test.txt".format(data_name)
+    dev_file = None
+    model_dir = _HOME_PATH + "/data/model"
+    word_embed_file = _HOME_PATH + "/data/embed/glove.6B.100d.txt"
+    vocab2id_file_pkl = _HOME_PATH + "/data/embed/{}_vocab2id.pkl".format(data_name)
+    we_file_pkl = _HOME_PATH + "/data/embed/{}_we.pkl".format(data_name)
+    log_dir = _HOME_PATH + "/log"
+
+    # [model]
+    we = None
+    model_name = "rcnn"
+    epoch_size = 5
+    batch_size = 128
+    word_dim = 100
+    max_seq_len = 40
+    rnn_size = 128
+    mlp_size = 50
+    class_num = 6
+    max_gradient_norm = 10
+    learning_rate = 1e-3
+    l2_rate = 1e-4
+
+
